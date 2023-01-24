2dfb40cf7d19164aa1928186568a23049bc56859
Shikhar Vashistha <51105234+shikharvashistha@users.noreply.github.com>
2023-01-24 19:59:22 +0000

    feat: dockerization & prisma postgres
    This commit dockerized the whole project, now we can use docker-compose run to make every component up and running & adds connection support to postgres via prisma ORM
    
    Signed-off-by: shikhar <51105234+shikharvashistha@users.noreply.github.com>

--- backend/dist/app.module.js
+++ backend/dist/app.module.js
@@ -8,13 +8,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const typeorm_1 = require("@nestjs/typeorm");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            config_1.ConfigModule.forRoot({ isGlobal: true }),
+            typeorm_1.TypeOrmModule.forRootAsync({
+                imports: [config_1.ConfigModule],
+                useFactory: (configService) => ({
+                    type: 'postgres',
+                    host: configService.get('DB_HOST'),
+                    port: +configService.get('DB_PORT'),
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_NAME'),
+                    entities: [],
+                    synchronize: true,
+                }),
+                inject: [config_1.ConfigService],
+            }),
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
---
+++ db/init.sql
@@ -0,0 +1,8 @@
+CREATE database db;
+
+CREATE TABLE "db" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
